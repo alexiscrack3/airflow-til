232fc460b75bd29622e8ca388ec962c7d7be1172
Alexis Ortega <alexis.oc3@gmail.com>
2024-09-14 10:48:18 -0600

    Create bash operator

---
+++ dags/02_bash_operator.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+dag = DAG(dag_id='bash_operator',
+        description="This DAG is used to test the BashOperator",
+        start_date=datetime(2024, 1, 1),
+        schedule_interval='@once')
+
+task = BashOperator(task_id='task1',
+                    bash_command='echo "Hello World"',
+                    dag=dag)
+task
