4f9e7b8f64b9723ffc0997f47ed73426807f06ce
Alexis Ortega <alexis.oc3@gmail.com>
2024-09-14 10:18:53 -0600

    Create empty operator

---
+++ dags/01_empty_operator.py
@@ -0,0 +1,11 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+dag = DAG(dag_id='empty_operator',
+        description='This DAG is used to test the EmptyOperator',
+        start_date=datetime(2024, 1, 1),
+        schedule_interval='@once')
+
+task1 = EmptyOperator(task_id='task1', dag=dag)
+task1
