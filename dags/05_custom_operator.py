51a0174834c7d1fa8be24b171cf492d6f1d56e2b
Alexis Ortega <alexis.oc3@gmail.com>
2024-09-14 16:45:56 -0600

    Create custom operator

---
+++ dags/05_custom_operator.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from hello_operator import HelloOperator
+
+dag = DAG(dag_id='custom_operator',
+        description='This DAG is used to test the custom operator',
+        start_date=datetime(2024, 1, 1),
+        schedule_interval='@once')
+
+task = HelloOperator(
+    task_id='task1',
+    name='World',
+    dag=dag)
+
+task
